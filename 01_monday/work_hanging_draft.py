dfbfcf4083eb297725e5208f9c0798aac4b77759
juliaschicker <juliaschicker@gmail.com>
2025-11-19 10:50:44 +0100

    Add Monday material.

---
+++ 01_monday/work_hanging_draft.py
@@ -0,0 +1,39 @@
+# Calculate the places for the nails to hang two same sized works on a wall.
+# l for length, h for height. Everything in cm.
+
+# Variables.
+numbers_of_works = 2
+wall_l = 500
+wall_h = 250
+work_l = 100
+work_h = 100
+distance_nail = 10 # the distance of the nail from edge of work
+distance_between_works = 20 # the distance between the two works
+
+# Calculations.
+# / divided by
+# * multiply
+
+# Calculate the total length of all the works using numbers_of_works, work_l dist_between_works
+# Fill this in
+# length_works_plus_distances = 
+# print("The length of all works plus distances is: " + str(length_works_plus_distances))
+
+# Calculate the distance from the side to the works
+# distance_side = 
+# print("The distance from the side to the work is: " + str(distance_side))
+
+# Calculate the position of the nail from the side
+# distance_nail_side = 
+# print("The distance of the outer nail from the side of the wall is: " + str(distance_nail_side))
+# / 2 here because we're halfing it, it's not dependent on the number of the works
+
+# Calculate the position of the top edge of the work to the ceiling
+# distance_top = 
+# print("The distance of the top edge of the work to the ceiling is: " + str(distance_top))
+
+# Calculate the position of the nail from the top
+# nail_top = 
+# print("The distance of the nail from the ceiling is: " + str(nail_top))
+
+# Try changing the size of the wall or work
